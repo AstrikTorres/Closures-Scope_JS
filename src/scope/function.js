2c7ff93dfd0794c0a184f7bd5b40449ff9499231
Astrik Torres <astriktorres@gmail.com>
2021-09-23 14:21:23 -0500

    Scope en Funciones

---
+++ src/scope/function.js
@@ -0,0 +1,9 @@
+const fruits = () => {
+    var fruit = 'apple';
+    console.log(fruit);
+};
+
+fruits();
+/* No se puede acceder a una variable declarada en una función por fuera:
+console.log(fruit);
+*/
