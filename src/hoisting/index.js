11a450116f88acc17fdcb8f37d140c2f3b1ab46c
Astrik Torres <astriktorres@gmail.com>
2021-10-21 12:02:12 -0500

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,19 @@
+a = 2;
+var a;
+console.log(a);
+// Print: 2
+
+/* -------------------- */
+
+console.log(a);
+var a = 2; 
+// Print: undefined
+
+/* -------------------- */
+
+nameOfDog('Sirius');
+
+function nameOfDog(name) {
+    console.log(name);
+}
+//Print: Sirius
